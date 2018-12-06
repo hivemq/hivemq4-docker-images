b4d718663ad7961e39b805d495b79f444eddda40
sbaier <simon.baier@dc-square.de>
2018-12-06 18:02:55 +0100

    add new entry point to dns image as well

--- hivemq4/dns-image/docker-entrypoint.sh
+++ hivemq4/dns-image/docker-entrypoint.sh
@@ -21,12 +21,33 @@ echo "set bind address from container hostname to ${ADDR}"
 export HIVEMQ_BIND_ADDRESS=${ADDR}
 
 # Step down from root privilege, only when we're attempting to run HiveMQ though.
-if [[ "$1" = '/opt/hivemq/bin/run.sh' && "$(id -u)" = '0' && "${HIVEMQ_NO_ROOT_STEP_DOWN}" != "true" ]]; then
-    # Restrict HiveMQ folder permissions
-    chown -R hivemq:hivemq /opt/hivemq*
-    chmod -R 750 /opt/hivemq*
-    HIVEMQ_BIND_ADDRESS=${ADDR} exec gosu hivemq "$BASH_SOURCE" "$@"
+if [[ "$1" = "/opt/hivemq/bin/run.sh" && "$(id -u)" = '0' && "${HIVEMQ_NO_ROOT_STEP_DOWN}" != "true" ]]; then
+    uid="hivemq"
+    gid="hivemq"
+    exec_cmd="exec gosu hivemq:hivemq"
+else
+    uid="$(id -u)"
+    gid="$(id -g)"
+    exec_cmd="exec"
 fi
 
-# Default if we're not running HiveMQ
-HIVEMQ_BIND_ADDRESS=${ADDR} exec "$@"
+readonly uid
+readonly gid
+readonly exec_cmd
+
+if [[ "$(id -u)" = "0" ]]; then
+    # Restrict HiveMQ folder permissions, non-recursive so we don't touch volumes
+    chown "${uid}":"${gid}" /opt/hivemq
+    chown "${uid}":"${gid}" /opt/hivemq-*
+    chown "${uid}":"${gid}" /opt/hivemq/data
+    chown "${uid}":"${gid}" /opt/hivemq/log
+    chown "${uid}":"${gid}" /opt/hivemq/conf
+    chown "${uid}":"${gid}" /opt/hivemq/license
+    # Recursive for bin, no volume here
+    chown -R "${uid}":"${gid}" /opt/hivemq/bin
+    chmod 700 /opt/hivemq*
+    chmod 700 /opt/hivemq-*
+    chmod -R 700 /opt/hivemq/bin
+fi
+
+HIVEMQ_BIND_ADDRESS=${ADDR} ${exec_cmd} "$@"
