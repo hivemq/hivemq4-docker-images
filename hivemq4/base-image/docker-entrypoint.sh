ce7f62c23c121f98af17b2a96fdc3c9ef1abe131
sbaier <simon.baier@dc-square.de>
2018-12-07 16:13:16 +0100

    cleanup, add chown for config.xml to prevent warning

--- hivemq4/base-image/docker-entrypoint.sh
+++ hivemq4/base-image/docker-entrypoint.sh
@@ -24,7 +24,7 @@ if [[ "$(id -u)" = "0" ]]; then
     chown "${uid}":"${gid}" /opt/hivemq/data
     chown "${uid}":"${gid}" /opt/hivemq/log
     chown "${uid}":"${gid}" /opt/hivemq/conf
-    chown "${uid}":"${gid}" /opt/hivemq/license
+    chown "${uid}":"${gid}" /opt/hivemq/conf/config.xml
     # Recursive for bin, no volume here
     chown -R "${uid}":"${gid}" /opt/hivemq/bin
     chmod 700 /opt/hivemq
