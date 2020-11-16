23665193302875f87787f40bf442e03ca4001082
Silvio Giebl <silvio.giebl@hivemq.com>
2020-11-16 13:52:40 +0100

    Replace cluster transport correctly

--- hivemq4/dns-image/pre-entry.sh
+++ hivemq4/dns-image/pre-entry.sh
@@ -2,10 +2,10 @@
 
 if [[ "${HIVEMQ_CLUSTER_TRANSPORT_TYPE}" == "UDP" ]]; then
     # shellcheck disable=SC2016
-    sed -i -e 's|--TRANSPORT_TYPE--|<udp><bind-address>${HIVEMQ_BIND_ADDRESS}</bind-address><bind-port>${HIVEMQ_CLUSTER_PORT}</bind-port><!-- disable multicast to avoid accidental cluster forming --><multicast-enabled>false</multicast-enabled></udp>|' /opt/hivemq/conf/config.xml
+    sed -i -e 's|<\!--TRANSPORT_TYPE-->|<udp><bind-address>${HIVEMQ_BIND_ADDRESS}</bind-address><bind-port>${HIVEMQ_CLUSTER_PORT}</bind-port><!-- disable multicast to avoid accidental cluster forming --><multicast-enabled>false</multicast-enabled></udp>|' /opt/hivemq/conf/config.xml
 elif [[ "${HIVEMQ_CLUSTER_TRANSPORT_TYPE}" == "TCP" ]]; then
     # shellcheck disable=SC2016
-    sed -i -e 's|--TRANSPORT_TYPE--|<tcp><bind-address>${HIVEMQ_BIND_ADDRESS}</bind-address><bind-port>${HIVEMQ_CLUSTER_PORT}</bind-port></tcp>|' /opt/hivemq/conf/config.xml
+    sed -i -e 's|<\!--TRANSPORT_TYPE-->|<tcp><bind-address>${HIVEMQ_BIND_ADDRESS}</bind-address><bind-port>${HIVEMQ_CLUSTER_PORT}</bind-port></tcp>|' /opt/hivemq/conf/config.xml
 fi
 
 exec /opt/docker-entrypoint.sh "$@"
