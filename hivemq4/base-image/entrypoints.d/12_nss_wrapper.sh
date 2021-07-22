78fd8d540675266e5d896de6f177c514a7d0e86d
sbaier <simon.baier@hivemq.com>
2021-07-22 20:34:04 +0200

    fix: gosu proper permissions for switching user on uid 0, add nss wrapper for re-mapping uid to user on arbitrary uid, switch to a docker-entrypoint.d approach for arbitrary endpoint scripts for easier debugging of issues.

---
+++ hivemq4/base-image/entrypoints.d/12_nss_wrapper.sh
@@ -0,0 +1,12 @@
+if ! getent passwd "$(id -u)" &> /dev/null && [ -e /usr/lib/libnss_wrapper.so ]; then
+    USER_ID=$(id -u)
+    GROUP_ID=$(id -g)
+    HOME=/opt/hivemq
+
+    grep -v -e ^hivemq -e "^$USER_ID" /etc/passwd > "$HOME/passwd"
+    echo "hivemq:x:${USER_ID}:${GROUP_ID}:HiveMQ:${HOME}:/bin/false" >> "$HOME/passwd"
+
+    export LD_PRELOAD=/usr/lib/libnss_wrapper.so
+    export NSS_WRAPPER_PASSWD=${HOME}/passwd
+    export NSS_WRAPPER_GROUP=/etc/group
+fi
