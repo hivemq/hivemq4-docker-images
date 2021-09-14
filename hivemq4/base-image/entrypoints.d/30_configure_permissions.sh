49898b7966e1874062d8379d6ba846fb5aa037da
sbaier <simon.baier@hivemq.com>
2021-09-14 10:52:16 +0200

    fix: invalid permissions on regular base-image run

--- hivemq4/base-image/entrypoints.d/30_configure_permissions.sh
+++ hivemq4/base-image/entrypoints.d/30_configure_permissions.sh
@@ -24,14 +24,14 @@ if [[ "$(id -u)" = "0" ]]; then
     chown "${uid}":"${gid}" /opt
     chown "${uid}":"${gid}" /opt/hivemq
     chown "${uid}":"${gid}" /opt/hivemq-*
-    chown "${uid}":"${gid}" /opt/hivemq/audit
-    chown "${uid}":"${gid}" /opt/hivemq/log
-    chown "${uid}":"${gid}" /opt/hivemq/conf
-    chown "${uid}":"${gid}" /opt/hivemq/conf/config.xml
-    chown "${uid}":"${gid}" /opt/hivemq/license
-    chown "${uid}":"${gid}" /opt/hivemq/backup
-    chown "${uid}":"${gid}" /opt/hivemq/tools
-    chown "${uid}":"${gid}" /opt/hivemq/extensions
+    chown -R "${uid}":"${gid}" /opt/hivemq/audit
+    chown -R "${uid}":"${gid}" /opt/hivemq/log
+    chown -R "${uid}":"${gid}" /opt/hivemq/conf
+    chown -R "${uid}":"${gid}" /opt/hivemq/conf/config.xml
+    chown -R "${uid}":"${gid}" /opt/hivemq/license
+    chown -R "${uid}":"${gid}" /opt/hivemq/backup
+    chown -R "${uid}":"${gid}" /opt/hivemq/tools
+    chown -R "${uid}":"${gid}" /opt/hivemq/extensions
     # Recursive for bin, no volume here
     chown -R "${uid}":"${gid}" /opt/hivemq/bin
     chmod 700 /opt/hivemq
