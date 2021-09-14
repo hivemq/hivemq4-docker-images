b2a6682a9e800549ce97cef43089b4d455bb5cbc
sbaier <simon.baier@hivemq.com>
2021-09-14 15:30:18 +0200

    fix: configure file permissions using find instead, remove unnecessary chmods

--- hivemq4/base-image/entrypoints.d/30_configure_permissions.sh
+++ hivemq4/base-image/entrypoints.d/30_configure_permissions.sh
@@ -4,7 +4,7 @@
 if [[ "$1" = "/opt/hivemq/bin/run.sh" && "$(id -u)" = '0' && "${HIVEMQ_NO_ROOT_STEP_DOWN}" != "true" ]]; then
     uid="hivemq"
     gid="hivemq"
-    exec_cmd="exec gosu hivemq:hivemq"
+    exec_cmd="exec gosu hivemq"
 else
     uid="$(id -u)"
     gid="$(id -g)"
@@ -16,26 +16,5 @@ readonly gid
 readonly exec_cmd
 
 if [[ "$(id -u)" = "0" ]]; then
-    # Restrict HiveMQ folder permissions, non-recursive so we don't touch volumes
-    chown "${uid}":"${gid}" /opt/hivemq/data
-    # Any of the following may fail but should still allow HiveMQ to start normally, so lets ignore errors
-    set +e
-    (
-    chown "${uid}":"${gid}" /opt
-    chown "${uid}":"${gid}" /opt/hivemq
-    chown "${uid}":"${gid}" /opt/hivemq-*
-    chown -R "${uid}":"${gid}" /opt/hivemq/audit
-    chown -R "${uid}":"${gid}" /opt/hivemq/log
-    chown -R "${uid}":"${gid}" /opt/hivemq/conf
-    chown -R "${uid}":"${gid}" /opt/hivemq/conf/config.xml
-    chown -R "${uid}":"${gid}" /opt/hivemq/license
-    chown -R "${uid}":"${gid}" /opt/hivemq/backup
-    chown -R "${uid}":"${gid}" /opt/hivemq/tools
-    chown -R "${uid}":"${gid}" /opt/hivemq/extensions
-    # Recursive for bin, no volume here
-    chown -R "${uid}":"${gid}" /opt/hivemq/bin
-    chmod 700 /opt/hivemq
-    chmod 700 /opt/hivemq-*
-    chmod -R 700 /opt/hivemq/bin
-    ) 2>/dev/null
+    find -L /opt \! -user hivemq -exec chown hivemq '{}' +
 fi
